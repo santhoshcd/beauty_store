080bccdb4384727f151d5f7131bdddebac717194
Santhosh <santhoshdevadiga.cs@gmail.com>
2018-01-03 14:50:49 +0000

    Add Spec for product and category model

---
+++ spec/controllers/api/auth_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe Api::AuthController, type: :controller do
+
+end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:category) }
+  it { should validate_presence_of(:name) }
+  it { should validate_uniqueness_of(:product_number) }
 end
