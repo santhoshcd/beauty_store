89076fc832674370c965e8fc7b3bf3dbc74c63a8
Santhosh <santhoshdevadiga.cs@gmail.com>
2018-01-04 15:42:49 +0000

    Update Test coverage

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,5 +3,4 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   it { should belong_to(:category) }
   it { should validate_presence_of(:name) }
-  it { should validate_uniqueness_of(:product_number) }
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,7 +21,7 @@ require 'shoulda/matchers'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
@@ -64,3 +64,4 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
+
